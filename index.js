ee6c2094739eca47e6ac7ef38a44ed5a6fc0c713
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-09-02 14:04:19 -0400

    Fix applied and all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+//TODO[x] - grab the dodger element
+const dodger = document.getElementById("dodger");
+ 
+//TODO[x] - Add right movement functionality
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+//FIXME[x] - Add right movement functionality. 
+// The fix was using style.left as the base for movement rather than style.right.
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+//TODO[x] - Adds event lister for left and right movement to the DOM 
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+
+  });
